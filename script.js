40e44da4c3d1b0ee11056c812b9c95285cae2775
Anwar Rahim <anwargithub@gmail.com>
2024-10-26 16:23:02 +0500

    new project Chrome extension creation

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 let myLeads = []
 
-const inputEl = document.getElementById("input-el").value
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
    // myLeads.push("Anwar Rahim")
    // console.log(myLeads)
+   myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
 
-
-
-
-
-
+for(let i =0; i < inputEl.length; i++){
+    // ulEl.textContent =
+    console.log(myLeads[i])
+    
+}
