15da656a766af3010c178c2a268a966d1b1caf71
Anwar Rahim <anwargithub@gmail.com>
2024-10-26 13:44:30 +0500

    new project Chrome extension creation

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 
-
+inputBtn.addEventListener("Click", function(){
+    console.log("hello working")
+})
 
 
 
